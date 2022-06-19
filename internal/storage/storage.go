31e6c5a7442a3a748836171ff9e5ce9104454dea
Tikhonin Fedor Igorevich <ftikhonin@ozon.ru>
2022-06-19 22:19:06 +0300

    some fix

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -18,10 +18,7 @@ func NewStorage() *Storage {
 }
 
 func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
-	//return file.NewFile(filename, blob)
-
 	newFile, err := file.NewFile(filename, blob)
-
 	if err != nil {
 		return nil, err
 	}
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -2,6 +2,6 @@ package storage
 
 import "github.com/ftikhonin/storage/internal/storage"
 
-func NewStorage() *storage.Storage {
+func NewStorage(someArg string) *storage.Storage {
 	return storage.NewStorage()
 }
